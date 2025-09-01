c87ba05e9d086e422bb6ae2c0f754cc6c8ac0f13
Reserve Code Source <rcs@itisuniqueofficial.com>
2025-09-01 12:46:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>URL Shortener</title>
-  <link rel="stylesheet" href="tool.css" />
+  <link rel="stylesheet" href="styles.css" />
 </head>
 <body>
   <main>
